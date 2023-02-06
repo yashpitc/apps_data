f92533b24038cb17e335cd1bf912b044bdcaf28a
YASH GANDHI <ygandhi@primaryitcare.com>
2023-02-07 03:35:30 +0530

    Dashboard Subjects Changes

--- lib/app/modules/dashboard/controllers/dashboard_controller.dart
+++ lib/app/modules/dashboard/controllers/dashboard_controller.dart
@@ -4,41 +4,84 @@ import 'package:get/get.dart';
 
 class DashboardController extends GetxController {
 
-  FirestoreService firestoreService = FirestoreService();
+ FirestoreService firestoreService = FirestoreService();
 
   @override
   void onInit() {
     super.onInit();
-    fetchSubjects();
+    fetchSubjectId();
   }
 
-  Future fetchSubjects() async {
-    List questionList = [];
+  Future<SubjectModel> fetchSubjectId() async {
+    List subjectIdList = [];
     try {
       await firestoreService.db
           .collection("apps")
-          .doc("mindfulness")
+          .doc("TestApp")
           .get()
           .then((DocumentSnapshot documentSnapshot) {
         if (documentSnapshot.exists) {
           List subjectList = documentSnapshot.get("subjects");
-          print("subjects:"+ subjectList.toString());
+         print("subjects:"+ subjectList.toString());
           for (var element in subjectList) {
-            questionList.add(element);
+          // subjectIdList.add(element);
+            return SubjectModel(subjectID: element);
+            print("element---" + subjectIdList[0].subjectID.toString());
+            // questionList.add(element);
           }
-          return questionList;
+          fetchSubjectsQuestions(subjectIdList);
+          // return questionList;
         } else {
-          return questionList;
+          //X return questionList;
+        }
+      });
+    } catch (e) {
+      return SubjectModel();
+    }
+    return SubjectModel();
+  }
+
+  Future fetchSubjectsQuestions(List SubjectIdList) async {
+    //List questionList = [];
+
+    try {
+      await firestoreService.db
+          .collection("subject")
+          .get()
+          .then((documentSnapshot) {
+        if (documentSnapshot != null) {
+          List subjectList = documentSnapshot.docs;
+          print("subjects:"+ subjectList.toString());
+          // for (var element in subjectList) {
+          //  return SubjectModel(subjectID: element);
+          //   // print("element" + subjectIds.toString());
+          //   // questionList.add(element);
+          // }
+         // return questionList;
+        } else {
+         //X return questionList;
         }
       });
     } catch (e) {
       return [];
     }
-    return questionList;
+   // return questionList;
   }
 
 
 
 
 
+}
+
+class SubjectModel {
+   int? subjectID;
+
+  SubjectModel({
+     this.subjectID,
+  });
+
+  SubjectModel.fromMap(DocumentSnapshot data) {
+    subjectID = data['subjet_id'];
+  }
 }
--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -1,5 +1,6 @@
 import 'package:AppsData/app/components/custom/custom_appbar.dart';
 import 'package:AppsData/app/components/custom/custom_textstyle.dart';
+import 'package:AppsData/app/modules/dashboard/controllers/dashboard_controller.dart';
 import 'package:AppsData/app/utils/constants/colors.dart';
 import 'package:AppsData/app/utils/constants/icons.dart';
 import 'package:AppsData/app/utils/constants/strings.dart';
@@ -30,60 +31,64 @@ class DashboardView extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.only(left: 18.0, right: 18.0),
-        child: Column(
-          children: [
-            Divider(
-              color: AppColors.blackColor,
-              thickness: 0.2,
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("TOTAL CHALLANAGES", style: CustomTextStyle.ChtitleStyle),
-                Text("60", style: CustomTextStyle.ChCountStyle),
-              ],
-            ),
-            SizedBox(height: 22),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              Text("COMPLETED CHALLENGES", style: CustomTextStyle.ChtitleStyle),
-              Text("23", style: CustomTextStyle.ChCountStyle),
-            ]),
-            SizedBox(height: 22),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              Text("LOST CHALLENGES", style: CustomTextStyle.ChtitleStyle),
-              Text("03", style: CustomTextStyle.ChCountStyle)
-            ]),
-            SizedBox(height: 40),
-            ChallengeCardView(),
-            SizedBox(height: 40),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                CustomRoundButton(
-                  icon: AppIcons.continueIcon,
-                  onTap: () {
-                    Get.toNamed(Routes.DAILYCHALLENGEVIEW);
-                  },
-                ),
-                CustomRoundButton(
-                  icon: AppIcons.refreshIcon,
-                  onTap: () {},
-                ),
-                CustomRoundButton(
-                  icon: AppIcons.historyIcon,
-                  onTap: () {
-                    Get.toNamed(Routes.PREVIOUSCHALLENGEVIEW);
-                  },
-                ),
-              ],
-            ),
-            SizedBox(height: 40),
-          ],
-        ),
+        child: GetBuilder<DashboardController>(
+        init: DashboardController(),
+          builder: (controller){
+            return Column(
+            children: [
+              Divider(
+                color: AppColors.blackColor,
+                thickness: 0.2,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("TOTAL CHALLANAGES", style: CustomTextStyle.ChtitleStyle),
+                  Text("60", style: CustomTextStyle.ChCountStyle),
+                ],
+              ),
+              SizedBox(height: 22),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Text("COMPLETED CHALLENGES", style: CustomTextStyle.ChtitleStyle),
+                Text("23", style: CustomTextStyle.ChCountStyle),
+              ]),
+              SizedBox(height: 22),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                Text("LOST CHALLENGES", style: CustomTextStyle.ChtitleStyle),
+                Text("03", style: CustomTextStyle.ChCountStyle)
+              ]),
+              SizedBox(height: 40),
+              ChallengeCardView(),
+              SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  CustomRoundButton(
+                    icon: AppIcons.continueIcon,
+                    onTap: () {
+                      Get.toNamed(Routes.DAILYCHALLENGEVIEW);
+                    },
+                  ),
+                  CustomRoundButton(
+                    icon: AppIcons.refreshIcon,
+                    onTap: () {},
+                  ),
+                  CustomRoundButton(
+                    icon: AppIcons.historyIcon,
+                    onTap: () {
+                      Get.toNamed(Routes.PREVIOUSCHALLENGEVIEW);
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 40),
+            ],
+          );
+        }
       ),
-    );
+    ));
   }
 }
